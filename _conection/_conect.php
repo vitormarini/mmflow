b8706c4edd7faee06fc6fa56b2e2ccda5a47aaaf
unknown <tamara_vindilino@hotmail.com>
2022-05-02 20:26:14 -0300

    upload Tamara

--- _conection/_conect.php
+++ _conection/_conect.php
@@ -30,7 +30,7 @@ $varBd["pass"] = "mttpocos";
 //exit;
 //$bd = novaConexao($varBd["sgbd"], $varBd["host"] , $varBd["base"], $varBd["user"], $varBd["pass"]);    
 // $bd = $objBDNovo = novaConexao("postgres", "179.188.16.134:5432", "base_mmflow", "base_mmflow", "mttpocos"); // Locaweb
-$bd = $objBDNovo = novaConexao("postgres", "ap5.cnd.hostlp.cloud:5432", "gestao_work", "postgres", "mttpocos"); // GHA
+$bd = $objBDNovo = novaConexao("postgres", "ap5.cnd.hostlp.cloud:5432", "gestao", "postgres", "mttpocos"); // GHA
 //$bd = $objBDNovo = novaConexao("postgres", "localhost", "gestao_work", "postgres", "mttpocos"); // GHA
 // $bd = $objBDNovo = novaConexao("postgres", "localhost", "gestao", "gestao", "mttpocos"); // Locaweb
 //$bd = $objBDNovo = novaConexao("postgres", "localhost", "gestao_teste", "postgres", "mttpocos");       
