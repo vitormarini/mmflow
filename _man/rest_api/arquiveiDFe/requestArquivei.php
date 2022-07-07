95f7301334d2e717c043b090c77e8decff0d3922
Vitor Hugo Marini <vitorhugomarini@MacBook-Air-de-Vitor.local>
2022-07-06 21:35:08 -0300

    Finalizaçao API Arquivei

--- _man/rest_api/arquiveiDFe/requestArquivei.php
+++ _man/rest_api/arquiveiDFe/requestArquivei.php
@@ -1,89 +1,102 @@
 <?php
 /**
  * Programa para integração das API`s Software Arquivei
- * Manual de Integração V1.9.pdf
+ * Documentação disposta pela Arquivei
+ * https://developers.arquivei.com.br/docs/get/v1/nfe/received
+ * Data: 06/07/2022
 */
 
-include_once 'requestBasicData.php';
-error_reporting(1);
+#Exemplo de chamada
+#$consultaNfe = consultaArquivei("100","50","nfe");  -- Retorno será um Array
 
-$inicial = ( isset($_GET['inicial']) && !empty($_GET['inicial']) ? $_GET['inicial'] : "0");
-$final   = ( isset($_GET['final'])   && !empty($_GET['final'])   ? $_GET['final']   : "50");
-$tipo    = ( isset($_GET['tipo'])    && !empty($_GET['tipo'])    ? $_GET['tipo']    : "nfe");
+/**
+ * #FUNÇAO PARA CONSULTA DOS ARQUIVOS CONTIDOS NA API - ARQUIVEI
+ * * Variáveis:
+ * @ - $xInicial : valor inicial, semelhante com o NSU
+ * @ - $xFinal   : valor final, semelhante ao LastNSU
+ * @ - $xTipo    : tipoda consulta ( TIPOS VÁLIDOS nfe, nfse )
+ */
+function consultaArquivei($xInicial, $xFinal, $xTipo){
 
-try {
-    #Dados para o Post 
-    $dataPost = "";
 
-    #Transformando o array em Json
-    $dataJson = json_encode($dataPost);
+    include_once 'requestBasicData.php';
+    error_reporting(1);
 
-    $endPoint      = "/v1/$tipo/received";
+    $inicial = ( isset($xInicial) && !empty($xInicial) ? $xInicial : "0");
+    $final   = ( isset($xFinal)   && !empty($xFinal)   ? $xFinal   : "50");
+    $tipo    = ( isset($xTipo)    && !empty($xTipo)    ? $xTipo    : "nfe");
 
-    #Dados do header
-    $headers = array(
-        "X-API-ID: {$xAPI_ID}",
-        "X-API-KEY: {$xAPI_KEY}",
-        "Content-Type: application/json"
-    );
+    try {
+        #Dados para o Post 
+        $dataPost = "";
 
-    #Definindo os limites da pesquisa
-    // $cursorLimit = "?cursor=$inicial&limit=$final";
-    $cursorLimit = "?from=2019-09-12&to=now&cursor=$inicial&limit=$final";
+        #Transformando o array em Json
+        $dataJson = json_encode($dataPost);
 
-    #Inicia a chamada do CURL
-    $curl = curl_init();
+        $endPoint      = "/v1/$tipo/received";
 
-    // Configura os parâmetros para a chamada
-    curl_setopt_array($curl, [
-        CURLOPT_URL => $urlRequest.$endPoint.$cursorLimit,    
-        CURLOPT_POST => false,
-        CURLOPT_RETURNTRANSFER => 1,    
-        CURLOPT_HTTPHEADER => $headers
-    ]);
-
-    // Armazena o retorno
-    $response = curl_exec($curl);
-
-    #Coleta informações do Curl
-    curl_getinfo($curl);
-
-    #Coleta os erros do retorno
-    curl_errno($curl);
-
-    #Encerra a conexão
-    curl_close($curl);
-
-    #Tratando o retorno
-    $dataRet = json_decode($response);
-
-    #Tratamento do retorno
-    if ( $dataRet->status->code == "200" ){
-
-        #Tratamento do retorno com SUCESSO!
-        $retorno = array(
-            "retorno_codigo"    => $codigoMensagem,
-            "retorno_mensagem"  => $dataRet->status->message,
-            "nextData"          => $dataRet->page->next,
-            "previusData"       => $dataRet->page->previous,
-            "countador"         => $dataRet->count,
-            "signature"         => $dataRet->signature,
-            "data"              => $dataRet->data
+        #Dados do header
+        $headers = array(
+            "X-API-ID: {$xAPI_ID}",
+            "X-API-KEY: {$xAPI_KEY}",
+            "Content-Type: application/json"
         );
-    }else{
-        #Tratamento para retornos que não tiveram sucesso!
-        $retorno = array(
-            "retorno_codigo"    => $codigoMensagem,
-            "retorno_mensagem"  => $dataRet->status->message
-        );
+
+        #Definindo os limites da pesquisa
+        // $cursorLimit = "?cursor=$inicial&limit=$final";
+        $cursorLimit = "?cursor=$inicial&limit=$final";
+        // $cursorLimit = "?from=2019-09-12&to=now&cursor=$inicial&limit=$final";
+
+        #Inicia a chamada do CURL
+        $curl = curl_init();
+
+        // Configura os parâmetros para a chamada
+        curl_setopt_array($curl, [
+            CURLOPT_URL => $urlRequest.$endPoint.$cursorLimit,    
+            CURLOPT_POST => false,
+            CURLOPT_RETURNTRANSFER => 1,    
+            CURLOPT_HTTPHEADER => $headers
+        ]);
+
+        // Armazena o retorno
+        $response = curl_exec($curl);
+
+        #Coleta informações do Curl
+        curl_getinfo($curl);
+
+        #Coleta os erros do retorno
+        curl_errno($curl);
+
+        #Encerra a conexão
+        curl_close($curl);
+
+        #Tratando o retorno
+        $dataRet = json_decode($response);
+
+        #Tratamento do retorno
+        if ( $dataRet->status->code == "200" ){
+
+            #Tratamento do retorno com SUCESSO!
+            $retorno = array(
+                "retorno_codigo"    => $codigoMensagem,
+                "retorno_mensagem"  => $dataRet->status->message,
+                "nextData"          => $dataRet->page->next,
+                "previusData"       => $dataRet->page->previous,
+                "countador"         => $dataRet->count,
+                "signature"         => $dataRet->signature,
+                "data"              => $dataRet->data
+            );
+        }else{
+            #Tratamento para retornos que não tiveram sucesso!
+            $retorno = array(
+                "retorno_codigo"    => $codigoMensagem,
+                "retorno_mensagem"  => $dataRet->status->message
+            );
+        }
+
+    }catch(Exception $e){
+        $retorno = "ERROR";    
     }
 
-    print"<pre>";
-    print_r($retorno);
-    exit;
-
-    $retorno = "SUCESSO";
-
-}catch(Exception $e){
-    $retorno = "ERROR";    
+    return $retorno;
 }
