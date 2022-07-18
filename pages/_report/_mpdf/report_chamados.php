764539d35858b2a6e5f01cd2604fda47ecc53704
Vitor Hugo Marini <vitorhugomarini@MacBook-Air-de-Vitor.local>
2022-07-18 16:37:22 -0300

    Report

--- pages/_report/_mpdf/report_chamados.php
+++ pages/_report/_mpdf/report_chamados.php
@@ -7,7 +7,7 @@ ini_set('memory_limit', '-1');
 ini_set('max_execution_time', 400);
 date_default_timezone_set('America/Sao_Paulo');
 
-include_once "/home/gestao/gestao.humanaalimentar.com.br/mpdf/Relatorio.class.php";
+include_once "../gestao/gestao.humanaalimentar.com.br/mpdf/Relatorio.class.php";
 
 ## Inicializando variáveis
 $p = json_decode($_POST['parametros']);
